6b70657ceb16c2ebf713192106d32bd5be56161d
Stephan Auerhahn <palp@palp.org>
2022-07-21 16:56:14 +0000

    latent + clip

--- predict.py
+++ predict.py
@@ -1,12 +1,17 @@
+from distutils.command.config import config
 import gc
 import io
 import math
+from pyexpat import model
 import sys
+from einops import rearrange
+import numpy as np
 import tempfile
 import threading
 import typing
 import queue
 
+from omegaconf import OmegaConf
 import clip
 import k_diffusion as K
 import lpips
@@ -20,7 +25,9 @@ from torchvision.transforms import functional as TF
 from tqdm import tqdm
 
 sys.path.append('./guided-diffusion')
+sys.path.append('./latent-diffusion')
 from guided_diffusion.script_util import create_model_and_diffusion, model_and_diffusion_defaults
+from ldm.util import instantiate_from_config
 
 from cog import BasePredictor, Input, Path
 
@@ -101,8 +108,34 @@ class GuidedDenoiserWithGrad(nn.Module):
         return cond_denoised
 
 class Predictor(BasePredictor):
-    def setup(self):
-        self.model_config = model_and_diffusion_defaults()
+
+    def LoadCompVisModel(self):
+        self.model_config = OmegaConf.load("/src/latent-diffusion/configs/latent-diffusion/txt2img-1p4B-eval.yaml")
+#        self.model_config.update({
+#            'attention_resolutions': '32, 16, 8',
+#            'class_cond': False,
+#            'diffusion_steps': 1000,
+#            'rescale_timesteps': True,
+#            'timestep_respacing': '1000',
+#            'learn_sigma': True,
+#            'noise_schedule': 'linear',
+#            'num_channels': 256,
+#            'num_head_channels': 64,
+#            'num_res_blocks': 2,
+#            'resblock_updown': True,
+#            'use_checkpoint': True,
+#            'use_fp16': True,
+#            'use_scale_shift_norm': True,
+#        })
+        self.model_config['image_size'] = 256
+        self.model_path = "/root/.cache/k-diffusion/latent_diffusion_txt2img_f8_large.ckpt"        
+        pl_sd = torch.load(self.model_path, map_location='cuda')
+        sd = pl_sd["state_dict"]
+        self.model = instantiate_from_config(self.model_config.model)
+        m, u = self.model.load_state_dict(sd, strict=False)                
+
+    def LoadOpenAIModel(self):
+        self.model_config = model_and_diffusion_defaults()        
         self.model_config.update({
             'attention_resolutions': '32, 16, 8',
             'class_cond': False,
@@ -120,13 +153,16 @@ class Predictor(BasePredictor):
             'use_scale_shift_norm': True,
         })
         self.model_config['image_size'] = 256
-        self.model_path = "/root/.cache/k-diffusion/256x256_diffusion_uncond.pt"
-        self.device = torch.device('cuda')
+        self.model_path = "/root/.cache/k-diffusion/256x256_diffusion_uncond.pt"        
         self.model, self.diffusion = create_model_and_diffusion(**self.model_config)
         self.model.load_state_dict(torch.load(self.model_path, map_location='cuda'))
+
+    def setup(self):        
+        self.device = torch.device('cuda')
+        self.LoadCompVisModel()        
         self.model.requires_grad_(False).eval().to(self.device)
-        if self.model_config['use_fp16']:
-            self.model.convert_to_fp16()
+        #if self.model_config['use_fp16']:
+        #    self.model.convert_to_fp16()
         
         self.clip_model = clip.load('ViT-B/16', jit=False)[0].eval().requires_grad_(False).to(self.device)
         self.clip_size = self.clip_model.visual.input_resolution
@@ -137,6 +173,7 @@ class Predictor(BasePredictor):
     def predict(
         self,
         text_prompt: str = Input(description="Prompt",default="A mysterious orb by Ernst Fuchs"),
+        #model: str = Input(description="Diffusion Model",default="latent_diffusion_txt2img_f8_large.ckpt", choices=['latent_diffusion_txt2img_f8_large.ckpt','256x256_diffusion_uncond.pt']),
         init_image: Path = Input(description="Initial image for the generation",default=None),
         sigma_start: int = Input(description="The starting noise level when using an init image", default=10),
         init_scale: int = Input(description="This enhances the effect of the init image, a good value is 1000.", default=1000),
@@ -151,6 +188,7 @@ class Predictor(BasePredictor):
         seed: int = Input(description="Seed (leave empty to use a random seed)", default=None, le=(2**32-1), ge=0),
     ) -> typing.Iterator[Path]:
         prompts = [text_prompt]
+        self.text_prompt = text_prompt
         image_prompts = []
         if (image_prompt):
             image_prompts = [str(image_prompt)]
@@ -158,7 +196,7 @@ class Predictor(BasePredictor):
             init_image = str(init_image)
         
         n_batches = 1
-                
+       
                 
         make_cutouts = MakeCutouts(self.clip_size, cutn, cut_pow)
         side_x = side_y = self.model_config['image_size']
@@ -200,44 +238,46 @@ class Predictor(BasePredictor):
             init = init.resize((side_x, side_y), Image.Resampling.LANCZOS)
             init = TF.to_tensor(init).to(self.device)[None] * 2 - 1
 
-        def cond_fn(x, sigma, denoised):
+        def cond_fn(x, sigma, denoised, cond):
             n = x.shape[0]
 
             # Anti-grain hack for the 256x256 ImageNet model
-            fac = sigma / (sigma ** 2 + 1) ** 0.5
-            denoised_in = x.lerp(denoised, fac)
+            #fac = sigma / (sigma ** 2 + 1) ** 0.5
+            #denoised_in = x.lerp(denoised, fac)
 
-            clip_in = self.normalize(make_cutouts(denoised_in.add(1).div(2)))
+            #clip_in = self.normalize(make_cutouts(denoised_in.add(1).div(2)))
+            clip_in = self.normalize(make_cutouts(model.decode_first_stage(denoised)))
             image_embeds = self.clip_model.encode_image(clip_in).float()
             dists = spherical_dist_loss(image_embeds[:, None], target_embeds[None])
             dists = dists.view([cutn, n, -1])
             losses = dists.mul(weights).sum(2).mean(0)
-            tv_losses = tv_loss(denoised_in)
+            tv_losses = tv_loss(denoised)
             range_losses = range_loss(denoised)
             loss = losses.sum() * clip_guidance_scale + tv_losses.sum() * tv_scale + range_losses.sum() * range_scale
             if init is not None and init_scale:
-                init_losses = self.lpips_model(denoised_in, init)
+                init_losses = self.lpips_model(denoised, init)
                 loss = loss + init_losses.sum() * init_scale
             return -torch.autograd.grad(loss, x)[0]
 
-        model_wrap = K.external.OpenAIDenoiser(self.model, self.diffusion, device=self.device)
-        sigmas = model_wrap.get_sigmas(n_steps)
+        #model_wrap = K.external.OpenAIDenoiser(self.model, self.diffusion, device=self.device)
+        self.model_wrap = K.external.CompVisDenoiser(self.model, False, device=self.device)
+        sigmas = self.model_wrap.get_sigmas(n_steps)
         if init is not None:
             sigmas = sigmas[sigmas <= sigma_start]
-        self.model_guided = GuidedDenoiserWithGrad(model_wrap, cond_fn)
+        self.model_guided = GuidedDenoiserWithGrad(self.model_wrap, cond_fn)
 
         output = queue.SimpleQueue()
 
         def callback(info):
-            if info['i'] % 50 == 0:
-                denoised = self.model_guided.orig_denoised
-                nrow = math.ceil(denoised.shape[0] ** 0.5)
-                grid = utils.make_grid(denoised, nrow, padding=0)
-                tqdm.write(f'Step {info["i"]} of {len(sigmas) - 1}:')                                
-                filename = f'step_{i}.png'
-                K.utils.to_pil_image(grid).save(filename)
-                output.put(filename)
-                #display.display(K.utils.to_pil_image(grid))
+            if info['i'] % 50 == 0:                
+            #    denoised = self.model_wrap.decode_first_stage(self.x)
+            #    nrow = math.ceil(denoised.shape[0] ** 0.5)
+            #    grid = utils.make_grid(denoised, nrow, padding=0)
+            #    tqdm.write(f'Step {info["i"]} of {len(sigmas) - 1}:')                                
+            #    filename = f'step_{i}.png'
+            #    K.utils.to_pil_image(grid).save(filename)
+            #    output.put(filename)
+            #    #display.display(K.utils.to_pil_image(grid))
                 tqdm.write(f'')
 
         if seed is not None:
@@ -263,14 +303,20 @@ class Predictor(BasePredictor):
         if (not self.success):
             raise RuntimeError('No output, check logs for errors')
         
-        for i, out in enumerate(self.samples):
+        samples = self.model.decode_first_stage(self.samples)
+        samples = torch.clamp((samples + 1.0) / 2.0, min=0.0, max=1.0)
+        
+        for i, out in enumerate(samples):            
+            sample = 255.0 * rearrange(out.cpu().numpy(), "c h w -> h w c")            
             filename = f'out_{i}.png'
-            K.utils.to_pil_image(out).save(filename)
+            Image.fromarray(sample.astype(np.uint8)).save(filename)
             yield Path(filename)
     
     def worker(self):
-        x = torch.randn([1, 3, self.side_y, self.side_x], device=self.device) * self.sigmas[0]
+        self.x = torch.randn([1, 4, self.side_y//8, self.side_x//8], device=self.device)
         if self.init is not None:
-            x += self.init
-        self.samples = K.sampling.sample_heun(self.model_guided, x, self.sigmas, second_order=False, s_churn=20, callback=self.callback)
+            self.x += self.init
+        n_samples = 1        
+        c = self.model.get_learned_conditioning(n_samples * [self.text_prompt])
+        self.samples = K.sampling.sample_heun(self.model_wrap, self.x, self.sigmas, second_order=False, s_churn=20, callback=self.callback, extra_args={"cond": c})
         self.success = True
